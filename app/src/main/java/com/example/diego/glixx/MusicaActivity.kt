1d1881bf2e1448f27e475e8c39a6ff57f3b3ab3f
diegitsen <diegitsen@gmail.com>
2018-01-26 17:11:15 -0500

    first 26

---
+++ app/src/main/java/com/example/diego/glixx/MusicaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.diego.glixx
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MusicaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_musica)
+    }
+}
